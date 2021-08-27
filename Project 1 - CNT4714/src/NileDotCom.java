7ebd1d1c7df0315d6a47f461fa97f34bd53dcf3e
Bradley Vanderzalm <bvan544@gmail.com>
2021-08-27 16:45:07 -0400

    Added a successful primary GUI. Setup methods and variables needed to add the main buttons, labels, and text fields.

--- Project 1 - CNT4714/src/NileDotCom.java
+++ Project 1 - CNT4714/src/NileDotCom.java
@@ -1,7 +1,67 @@
+import javax.swing.*;
+
 public class NileDotCom
 {
+    private static JPanel websitePanel;
+    private static JFrame websiteFrame;
+
+    private static JLabel numItems;
+    private static JLabel itemID;
+    private static JLabel itemQuantity;
+    private static JLabel itemInfo;
+    private static JLabel orderSubtotal;
+
+    private static JTextField numItemsText;
+    private static JTextField itemIDText;
+    private static JTextField itemQuantityText;
+    private static JTextField itemInfoText;
+    private static JTextField orderSubtotalText;
+
+    private static JButton processItemButton;
+    private static JButton confirmItemButton;
+    private static JButton viewOrderButton;
+    private static JButton finishOrderButton;
+    private static JButton newOrderButton;
+    private static JButton exitButton;
+
+    public void runWebsite()
+    {
+        openPrimaryGUI();
+        setUpLabelInstructions();
+        setUpUserTextFields();
+        setUpButtons();
+        websiteFrame.setVisible(true);
+    }
+
+    public void openPrimaryGUI()
+    {
+        websitePanel = new JPanel();
+        websiteFrame = new JFrame();
+        websiteFrame.setSize(800, 400);
+        websiteFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        websiteFrame.add(websitePanel);
+
+        websitePanel.setLayout(null);
+    }
+
+    public void setUpLabelInstructions()
+    {
+
+    }
+
+    public void setUpUserTextFields()
+    {
+
+    }
+
+    public void setUpButtons()
+    {
+
+    }
+
     public static void main(String [] args)
     {
-        
+        NileDotCom nile = new NileDotCom();
+        nile.runWebsite();
     }
 }
