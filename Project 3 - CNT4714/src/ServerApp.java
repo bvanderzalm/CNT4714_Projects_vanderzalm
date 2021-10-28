e069d089e5fcfd9f1d1c231434431749c0b12ee6
Bradley Vanderzalm <bvan544@gmail.com>
2021-10-28 05:25:00 -0400

    Added main visual aspects of GUI.

--- Project 3 - CNT4714/src/ServerApp.java
+++ Project 3 - CNT4714/src/ServerApp.java
@@ -9,11 +9,21 @@ import java.sql.SQLException;
 public class ServerApp extends JFrame
 {
     static final String DEFAULT_QUERY = "";
+    private final String DEFAULT_DATABASE_URL = "jdbc:mysql://localhost:3306/project3?useTimezone=true&serverTimezone=UTC";
+    private final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private String driverStringList [] = {DEFAULT_DRIVER, ""}, databaseURLStringList [] = {DEFAULT_DATABASE_URL, ""};
     private ResultSetTableModel tableModel;
     private JTextArea queryArea;
     private JScrollPane scrollPane;
     private JButton sqlExecuteCommandButton, sqlClearCommandButton, connectToDatabaseButton, clearResultWindowButton;
+    private JLabel driverLabel, databaseURLLabel, usernameLabel, passwordLabel, databaseConnectionLabel;
+    private JComboBox driverComboBox, databaseURLComboBox;
+    private JTextField usernameTextField;
+    private JPasswordField passwordField;
     private Box queryBox;
+    private JPanel userTextFieldsAndLabelsPanel, sqlCommandButtonsPanel, dbConnectButtonAndLabelPanel;
+    private JPanel northComponents, centerComponents, southComponents;
+
     private JTable resultTable;
 
     public ServerApp()
@@ -21,31 +31,45 @@ public class ServerApp extends JFrame
         super("Project 3 - SQL Client App - (BV - CNT 4714 - Fall 2021)");
         try
         {
-            tableModel = new ResultSetTableModel(DEFAULT_QUERY);
+//            tableModel = new ResultSetTableModel(DEFAULT_QUERY);
             setUpQueryTextArea();
+            setUpTextFieldsAndLabels();
             setUpButtons();
-            setUpQueryBox();
-            resultTable = new JTable(tableModel);
-            resultTable.setGridColor(Color.BLACK);
 
-            add(queryBox, BorderLayout.NORTH);
-            add(new JScrollPane(resultTable), BorderLayout.CENTER);
+
+
+
+            //setUpQueryBox();
+
+            //tableModel = new ResultSetTableModel();
+            //resultTable = new JTable(tableModel);
+            //resultTable.setGridColor(Color.BLACK);
+
+            placeItemsUsingPanels();
+
+            //add(queryBox);//, BorderLayout.NORTH);
+            //add(new JScrollPane(resultTable), BorderLayout.CENTER);
             setUpButtonActionListeners();
-            setSize(600, 300);
+            setSize(800, 500);
             setVisible(true);
 
         }
 
-        catch (ClassNotFoundException classNotFound)
-        {
-            popUpErrorMessage("MySQL driver not found", "Driver not found");
-        }
+//        catch (ClassNotFoundException classNotFound)
+//        {
+//            popUpErrorMessage("MySQL driver not found", "Driver not found");
+//        }
+//
+//        catch (SQLException sqlException)
+//        {
+//            popUpErrorMessage(sqlException.getMessage(), "Database error");
+//            //******************************************************************************************
+//            //tableModel.disconnectFromDatabase();
+//        }
 
-        catch (SQLException sqlException)
+        catch (Exception ex)
         {
-            popUpErrorMessage(sqlException.getMessage(), "Database error");
-            //******************************************************************************************
-            //tableModel.disconnectFromDatabase();
+            popUpErrorMessage(ex.getMessage(), "Error");
         }
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -54,7 +78,7 @@ public class ServerApp extends JFrame
         {
             public void windowClosed(WindowEvent event)
             {
-                tableModel.disconnectFromDatabase();
+                //tableModel.disconnectFromDatabase();
                 System.exit(0);
             }
         }
@@ -63,28 +87,29 @@ public class ServerApp extends JFrame
 
     public void executeSQLCommand()
     {
-        try
-        {
-            tableModel.setQuery(queryArea.getText());
-        }
-
-        catch (SQLException sqlException)
-        {
-            popUpErrorMessage(sqlException.getMessage(), "Database error");
-
-            try
-            {
-                tableModel.setQuery(DEFAULT_QUERY);
-                queryArea.setText(DEFAULT_QUERY);
-            }
-
-            catch (SQLException sqlException2)
-            {
-                popUpErrorMessage(sqlException2.getMessage(), "Database error");
-                //***************************************************************************************************************************************
-//                tableModel.disconnectFromDatabase();
-            }
-        }
+        System.out.println("Execute SQLCommand");
+//        try
+//        {
+//            tableModel.setQuery(queryArea.getText());
+//        }
+//
+//        catch (SQLException sqlException)
+//        {
+//            popUpErrorMessage(sqlException.getMessage(), "Database error");
+//
+//            try
+//            {
+//                tableModel.setQuery(DEFAULT_QUERY);
+//                queryArea.setText(DEFAULT_QUERY);
+//            }
+//
+//            catch (SQLException sqlException2)
+//            {
+//                popUpErrorMessage(sqlException2.getMessage(), "Database error");
+//                //***************************************************************************************************************************************
+////                tableModel.disconnectFromDatabase();
+//            }
+//        }
     }
 
     public void popUpErrorMessage(String msg, String title)
@@ -93,7 +118,8 @@ public class ServerApp extends JFrame
     }
 
     // Sets up JTextArea in which user types queries.
-    public void setUpQueryTextArea() {
+    public void setUpQueryTextArea()
+    {
         queryArea = new JTextArea(DEFAULT_QUERY, 3, 100);
         queryArea.setWrapStyleWord(true);
         queryArea.setLineWrap(true);
@@ -102,22 +128,129 @@ public class ServerApp extends JFrame
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 
-    public void setUpButtons() {
+    public void setUpTextFieldsAndLabels()
+    {
+        userTextFieldsAndLabelsPanel = new JPanel(new GridLayout(4, 2));
+        setUpJDBCDriverLabelAndComboBox();
+        setUpDatabaseURLLabelAndComboBox();
+        setUpUsernameLabelAndTextField();
+        setUpPasswordLabelAndTextField();
+        setUpDatabaseConnectionLabel();
+    }
+
+    public void setUpButtons()
+    {
+        sqlCommandButtonsPanel = new JPanel(new GridLayout(1, 4));
         setUpClearSQLCommandButton();
         setUpExecuteSQLCommandButton();
         setUpConnectToDatabaseButton();
         setUpClearResultWindowButton();
+
+
     }
 
-    public void setUpQueryBox()
+//    public void setUpQueryBox()
+//    {
+//        queryBox = Box.createHorizontalBox();
+//        queryBox.add(scrollPane);
+//        queryBox.add(sqlClearCommandButton);
+//        queryBox.add(sqlExecuteCommandButton);
+//        queryBox.setBounds(400, 200, 230, 50);
+////        box.add(connectToDatabaseButton);
+////        box.add(clearResultWindowButton);
+//    }
+
+    public void placeItemsUsingPanels()
     {
-        queryBox = Box.createHorizontalBox();
-        queryBox.add(scrollPane);
-        queryBox.add(sqlClearCommandButton);
-        queryBox.add(sqlExecuteCommandButton);
-//        box.add(connectToDatabaseButton);
-//        box.add(clearResultWindowButton);
+        northComponents = new JPanel(new GridLayout(1, 2));
+        northComponents.add(userTextFieldsAndLabelsPanel);
+        northComponents.add(queryArea);
+
+        centerComponents = new JPanel();
+//        centerComponents.add(sqlCommandButtonsPanel, BorderLayout.EAST);
+//        centerComponents.add(dbConnectButtonAndLabelPanel, BorderLayout.SOUTH);
+
+//        centerComponents.add(dbConnectButtonAndLabelPanel, BorderLayout.EAST);
+        centerComponents.add(sqlCommandButtonsPanel, BorderLayout.SOUTH);
+
+        southComponents = new JPanel();
+        southComponents.setLayout(new BorderLayout(20, 0));
+        //south.add(scrollpane) borderlayout north
+        southComponents.add(dbConnectButtonAndLabelPanel, BorderLayout.NORTH);
+        southComponents.add(clearResultWindowButton, BorderLayout.SOUTH);
+
+        add(northComponents, BorderLayout.NORTH);
+        add(centerComponents, BorderLayout.CENTER);
+        add(southComponents, BorderLayout.SOUTH);
     }
+
+
+
+    //******************** Setup for labels and TextFields********************
+
+    public void setUpJDBCDriverLabelAndComboBox()
+    {
+        // Label
+        driverLabel = new JLabel("JDBC Driver");//, SwingConstants.LEFT);
+        driverLabel.setBackground(Color.GRAY);
+        driverLabel.setForeground(Color.BLACK);
+        driverLabel.setOpaque(true);
+//        driverLabel.setBounds(10, 20, 23, 25);
+//        add(driverLabel);//, BorderLayout.WEST);
+
+        // ComboBox (Textfield with different choices)
+        driverComboBox = new JComboBox(driverStringList);
+
+        userTextFieldsAndLabelsPanel.add(driverLabel);
+        userTextFieldsAndLabelsPanel.add(driverComboBox);
+    }
+
+    public void setUpDatabaseURLLabelAndComboBox()
+    {
+        databaseURLLabel = new JLabel("Database URL");//, SwingConstants.LEFT);
+        databaseURLLabel.setBackground(Color.GRAY);
+        databaseURLLabel.setForeground(Color.BLACK);
+        databaseURLLabel.setOpaque(true);
+//        databaseURLLabel.setBounds(10, 40, 23, 25);
+//        add(databaseURLLabel);//, BorderLayout.WEST);
+
+        databaseURLComboBox = new JComboBox(databaseURLStringList);
+
+        userTextFieldsAndLabelsPanel.add(databaseURLLabel);
+        userTextFieldsAndLabelsPanel.add(databaseURLComboBox);
+    }
+
+    public void setUpUsernameLabelAndTextField()
+    {
+        usernameLabel = new JLabel("Username");
+        usernameLabel.setBackground(Color.GRAY);
+        usernameLabel.setForeground(Color.BLACK);
+        usernameLabel.setOpaque(true);
+        usernameTextField = new JTextField();
+        userTextFieldsAndLabelsPanel.add(usernameLabel);
+        userTextFieldsAndLabelsPanel.add(usernameTextField);
+    }
+
+    public void setUpPasswordLabelAndTextField()
+    {
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBackground(Color.GRAY);
+        passwordLabel.setForeground(Color.BLACK);
+        passwordLabel.setOpaque(true);
+        passwordField = new JPasswordField();
+        userTextFieldsAndLabelsPanel.add(passwordLabel);
+        userTextFieldsAndLabelsPanel.add(passwordField);
+    }
+
+    public void setUpDatabaseConnectionLabel()
+    {
+        databaseConnectionLabel = new JLabel("No Connection Now.");
+        databaseConnectionLabel.setBackground(Color.BLACK);
+        databaseConnectionLabel.setForeground(Color.RED);
+        databaseConnectionLabel.setOpaque(true);
+    }
+
+    //******************** Setup for Buttons ********************
 
     public void setUpClearSQLCommandButton() {
         sqlClearCommandButton = new JButton("Clear SQL Command");
@@ -125,6 +258,9 @@ public class ServerApp extends JFrame
         sqlClearCommandButton.setForeground(Color.RED);
         sqlClearCommandButton.setBorderPainted(false);
         sqlClearCommandButton.setOpaque(true);
+        //sqlClearCommandButton.setBounds(400, 200, 230, 25);
+        //add(sqlClearCommandButton);
+        sqlCommandButtonsPanel.add(sqlClearCommandButton);
     }
 
     public void setUpExecuteSQLCommandButton() {
@@ -133,6 +269,9 @@ public class ServerApp extends JFrame
         sqlExecuteCommandButton.setForeground(Color.BLACK);
         sqlExecuteCommandButton.setBorderPainted(false);
         sqlExecuteCommandButton.setOpaque(true);
+//        sqlExecuteCommandButton.setBounds(450, 200, 230, 25);
+//        add(sqlExecuteCommandButton);
+        sqlCommandButtonsPanel.add(sqlExecuteCommandButton);
     }
 
     public void setUpConnectToDatabaseButton()
@@ -142,6 +281,10 @@ public class ServerApp extends JFrame
         connectToDatabaseButton.setForeground(Color.YELLOW);
         connectToDatabaseButton.setBorderPainted(false);
         connectToDatabaseButton.setOpaque(true);
+
+        dbConnectButtonAndLabelPanel = new JPanel(new GridLayout(1, 5));
+        dbConnectButtonAndLabelPanel.add(connectToDatabaseButton);
+        dbConnectButtonAndLabelPanel.add(databaseConnectionLabel);
     }
 
     public void setUpClearResultWindowButton()
@@ -151,6 +294,7 @@ public class ServerApp extends JFrame
         clearResultWindowButton.setForeground(Color.BLACK);
         clearResultWindowButton.setBorderPainted(false);
         clearResultWindowButton.setOpaque(true);
+//        clearResultWindowButton.getPreferredSize();
     }
 
     public void setUpButtonActionListeners()
@@ -184,7 +328,12 @@ public class ServerApp extends JFrame
 
 
             }
-        }
+        };
+
+        sqlExecuteCommandButton.addActionListener(buttonActionListener);
+        sqlClearCommandButton.addActionListener(buttonActionListener);
+        connectToDatabaseButton.addActionListener(buttonActionListener);
+        clearResultWindowButton.addActionListener(buttonActionListener);
     }
 
     public static void main(String [] args)
